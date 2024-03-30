b5f9ac7b6523b376ed81cb6e8d09e926941878a6
ashan <uhashan17929@gmail.com>
2024-03-30 17:18:14 +0530

    Add Hello in text

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
                 <p className='py-4 pl-2 text-xl font-bold md:text-5xl sm:text-4xl md:pl-4'>Fast, Flexible financing for</p>
                 {/* <Typed sting={['BTB', 'BTC','SAS']} typeSpeed={120} backSpeed={140}/> */}
             </div>
-            <p className='text-xl font-bold text-gray-500 md:text-2xl'>Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd</p>
+            <p className='text-xl font-bold text-gray-500 md:text-2xl'>Hello Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd</p>
             <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black '>Get Started</button>
         </div>
     </div>
